4b8c6509935cd70daa2a6445c736f6605d3d967a
Gerrald <gerrald@weelden.net>
2025-11-19 15:51:00 +0100

    Working assignment 4.2.2

---
+++ lib/plot/timeFrequencyPlot.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+from matplotlib import axes
+from scipy.fft import fft, fftshift
+import numpy as np
+
+def timeFrequencyPlot(x: list|np.ndarray, Fs: int, time_ax: axes.Axes, freq_ax: axes.Axes, time_title:str=None, freq_title:str=None, grid: bool = True, samples_offset: float = 0, apply_fftshift:bool=False, resolution: int|None = None):
+    if resolution is not None:
+        X = fft(x, resolution)
+    else:
+        X = fft(x)
+    
+    t = np.linspace(0 + samples_offset/Fs, (len(x) + samples_offset)/Fs, len(x))
+    
+    if apply_fftshift:
+        X = fftshift(X)
+        f = np.linspace(-Fs/2, Fs/2, len(X))
+    else:
+        f = np.linspace(0, Fs, len(X))
+    
+    time_ax.plot(t, x)
+    time_ax.set_xlabel("Time [s]")
+    time_ax.set_ylabel("Amplitude")
+    
+    freq_ax.plot(f, np.abs(X))
+    freq_ax.set_xlabel("Frequency [Hz]")
+    freq_ax.set_ylabel("Amplitude")
+    
+    time_ax.grid(grid)
+    freq_ax.grid(grid)
+    
+    if time_title is not None:
+        time_ax.set_title(time_title)
+    if freq_title is not None:
+        freq_ax.set_title(freq_title)
--- src/module_1/main.py
+++ src/module_1/main.py
@@ -1,7 +1,27 @@
 from config import *
+from scipy import signal
+from scipy.fft import fft
+import numpy as np
+import matplotlib.pyplot as plt
+from lib.plot.timeFrequencyPlot import *
+from math import floor
+
+def construct_filter(low: float, high: float, Fs: int, order: int = 2):
+    b, a = signal.butter(order, [2*low/Fs, 2*high/Fs], btype="band")
+    g = signal.filtfilt(b, a, [*np.zeros(10000), 1, *np.zeros(10000)])
+    return g
+
+def filter(b, a, data):
+    return signal.filtfilt(b, a, data)
 
 def main():
-    print("hello world")
+    Fs = 48e3
+    g = construct_filter(10, 800, Fs)
+
+    fig, ax = plt.subplots(1, 2, figsize=(8,4), constrained_layout=True)
+    timeFrequencyPlot(g, Fs, ax[0], ax[1], samples_offset = -len(g)/2, apply_fftshift=True, resolution=None)
+    fig.suptitle("Non-causal butterworth bandpass filter $g(t)$")
+    plt.show()
 
 if __name__ == "__main__":
     main()
