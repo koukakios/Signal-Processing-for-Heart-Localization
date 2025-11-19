377f862327c10b330069d8e0287908bcd7cb7304
Gerrald <gerrald@weelden.net>
2025-11-19 09:51:20 +0100

    Added assignments folder; begin beamforming assignment

--- src/module_3/beamforming.py
+++ src/module_3/beamforming.py
@@ -1,6 +1,19 @@
+
+# array response, depends on angle of arrival
 def a_lin(theta, M, d, v, f0):
-    
-    pass
+    """Returns the *array response* or *steering vector* for a Uniform Linear Microphone Array
+
+    Args:
+        theta (float): angle of arrival
+        M (int): number of microphones
+        d (float): distance between microphones (m)
+        v (float): speed of sound (m/s)
+        f0 (float): frequency of wave (Hz)
+
+    Returns:
+        np.ndarray: The array response
+    """    
+    return theta
 
 if __name__ == "__main__":
     pass
