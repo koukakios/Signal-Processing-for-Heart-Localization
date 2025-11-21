d9fb4b919c3614f63d5d786aabb99f12b4583267
Kostas Koukakis <kkoukakis@tudelft.nl>
2025-11-21 09:41:35 +0100

    changes in the a_lin funct

--- src/module_3/beamforming.py
+++ src/module_3/beamforming.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib as plt
 # array response, depends on angle of arrival
 def a_lin(theta, M, d, v, f0):
     """Returns the *array response* or *steering vector* for a Uniform Linear Microphone Array
@@ -14,9 +15,20 @@ def a_lin(theta, M, d, v, f0):
         np.ndarray: The array response
     """  
     """okay"""
-    result = np.array([ np.exp(np.imag(-j*mic*(d/v)*np.sin(theta)*2*np.pi*f0)) for mic in range (M)])
+    result = np.array([ np.exp(np.imag(0 -1*mic*(d/v)*np.sin(theta)*2*np.pi*f0 *1j)) for mic in range (M)])
 
     return result
+
+def test_a_lin():
+    theta = 30
+    M = 3
+    d = 1
+    v = 343
+    f0 = 250
+
+    array_response_vector = a_lin(theta, M, d, v, f0)
+    
+    print (abs(array_response_vector))
 
 if __name__ == "__main__":
     pass
--- src/module_3/main.py
+++ src/module_3/main.py
@@ -1,4 +1,8 @@
+import beamforming
+from beamforming import a_lin
+from beamforming import test_a_lin
 def main():
+    test_a_lin()
     pass
 
 if __name__ == "__main__":
