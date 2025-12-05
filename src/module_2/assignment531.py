f61f14f05183df4bd378b5a81e509857d36e9778
rubC0d3r <rubenjochemsen@gmail.com>
2025-12-05 11:09:46 +0100

    delays and gains
    delays and gains calculated for each valve looping over the mics

---
+++ src/module_2/assignment531.py
@@ -0,0 +1,59 @@
+import numpy as np
+from lib.config.ConfigParser import ConfigParser
+
+
+
+valve_locs = {"M":np.array((6.37,10.65,6.00)),
+              "T":np.array((0.94,9.57,5.5)),
+              "A":np.array((5.5,11.00,3.6)),
+              "P":np.array((3.9,11.5,4.5))}
+
+valve_locs = np.array([(6.37,10.65,6.00), # M
+              (0.94,9.57,5.5), # T
+              (5.5,11.00,3.6), # A
+              (3.9,11.5,4.5)]) # P
+
+mic_locs = {0: np.array((2.5, 5, 0)),
+            1: np.array((2.5, 10, 0)),
+            2: np.array((2.5, 15, 0)),
+            3: np.array((7.5, 5, 0)),
+            4: np.array((7.5, 10, 0)),
+            5: np.array((7.5, 15, 0))}
+
+mic_locs = np.array([(2.5, 5, 0), # 0
+            (2.5, 10, 0), # 1
+            (2.5, 15, 0), # 2
+            (7.5, 5, 0), # 3
+            (7.5, 10, 0), # 4
+            (7.5, 15, 0)]) # 5
+
+# for i in range(6):
+#     mic_locs[i] = (2.5+(i//3)*5,(i%3)*5+5,0))
+
+
+
+
+
+def threeD_model():
+    config = ConfigParser()
+    
+    
+    
+    
+    
+    for mic_loc in mic_locs:
+        new_mic_loc = np.tile(mic_loc,(4,1)) # repeat the mic loc, to comply with the dimensions of np.linalg.norm
+        
+        dists_to_valves = np.linalg.norm(valve_locs-new_mic_loc, axis=1)
+        
+        delays = dists_to_valves/config.Multichannel.V_body
+        
+        gain_constant = 1
+        gains = gain_constant/dists_to_valves
+        
+        print(delays)
+        break
+
+
+if __name__ == "__main__":
+    threeD_model()
