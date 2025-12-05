a20357e4cf0bd199a4ad2c450b23c379d743f7b5
Kostas Koukakis <kkoukakis@tudelft.nl>
2025-12-05 12:03:49 +0100

    7.1.4 module 4 working

--- src/module_3/DoA_find.py
+++ src/module_3/DoA_find.py
@@ -6,26 +6,40 @@ from scipy.signal.windows import gaussian
 from scipy.io import wavfile
 from pathlib import Path
 import matplotlib.pyplot as plt
-
+import soundfile as sf
+#README FOR REPORT. So like 
 
 if __name__ == "__main__":
     fs = 48000
     win = ('gaussian', 1e-2 * fs)
     SFT = ShortTimeFFT.from_window(win, fs, nperseg = 256 ,noverlap=0, scale_to='magnitude', phase_shift=None)
-    filepath1 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-0-degrees\recording_2024-09-30_12-48-30_channel_1.wav")
-    filepath2 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-0-degrees\recording_2024-09-30_12-48-30_channel_2.wav")
-    filepath3 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-0-degrees\recording_2024-09-30_12-48-30_channel_3.wav")
-    filepath4 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-0-degrees\recording_2024-09-30_12-48-30_channel_4.wav")
-    filepath5 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-0-degrees\recording_2024-09-30_12-48-30_channel_5.wav")
-    filepath6 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-0-degrees\recording_2024-09-30_12-48-30_channel_6.wav")
-    rate, signal1 = wavfile.read(filepath1)
-    rate, signal2 = wavfile.read(filepath2)
-    rate, signal3 = wavfile.read(filepath3)
-    rate, signal4 = wavfile.read(filepath4)
-    rate, signal5 = wavfile.read(filepath5)
-    rate, signal6 = wavfile.read(filepath6)
+    path2source = Path(r"C:\Users\kkouk\IP3\2 source, distance 7 meter, microphone stand at 0 degrees, speaker at 7 degrees left and right.wav")
+    #filepath1 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-30-degrees\recording_2024-09-30_12-55-47_channel_1.wav")
+    #filepath2 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-30-degrees\recording_2024-09-30_12-55-47_channel_2.wav")
+    #filepath3 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-30-degrees\recording_2024-09-30_12-55-47_channel_3.wav")
+    #filepath4 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-30-degrees\recording_2024-09-30_12-55-47_channel_4.wav")
+    #filepath5 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-30-degrees\recording_2024-09-30_12-55-48_channel_5.wav")
+    #filepath6 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-30-degrees\recording_2024-09-30_12-55-48_channel_6.wav")
+    
+    sources, rate = sf.read(path2source)
+    #rate, signal1 = wavfile.read(filepath1)
+    #rate, signal2 = wavfile.read(filepath2)
+    #rate, signal3 = wavfile.read(filepath3)
+    #rate, signal4 = wavfile.read(filepath4)
+    #rate, signal5 = wavfile.read(filepath5)
+    #rate, signal6 = wavfile.read(filepath6)
+    print ("ayo")
+    print (sources.shape)
+    #print(signal1.shape)
 
-    print(signal1.shape)
+    signal1 = sources[:,0]
+    signal2 = sources[:,1]
+    signal3 = sources[:,2]
+    signal4 = sources[:,3]
+    signal5 = sources[:,4]
+    signal6 = sources[:,5]
+    
+
     
     Sx1 = SFT.stft(signal1)
     Sx2 = SFT.stft(signal2)
@@ -35,18 +49,23 @@ if __name__ == "__main__":
     Sx6 = SFT.stft(signal6)
 
     Sx_all=np.stack((Sx1,Sx2,Sx3,Sx4,Sx5,Sx6))
+    
     #print (Sx1.shape)
     print(Sx_all.shape)
+
     f_bins = SFT.f
-    print(f_bins[1] - f_bins[0])
-    X = Sx_all[:, 8, :]
+
+    Delta_f = f_bins[1] - f_bins[0]
+    print( Delta_f)
+    bin = 9
+    X = Sx_all[:,bin , :]
     print(X.shape) 
     Rx = np.cov(X)
-
+    
     M = 6
     d = 0.1
     v = 343
-    f0 = f_bins[8]
+    f0 = f_bins[bin]
     theta_range = np.array([i for i in range (-90,90)])
     pspec = MVDR(theta_range, M, d, v, f0, Rx)
     print ("done with MVDR")
---
+++ src/module_4/7.1.4_exercise.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+
+def a_lin(theta, M, d, v, f0):
+    """Returns the *array response* or *steering vector* for a Uniform Linear Microphone Array
+
+    Args:
+        theta (float): angle of arrival
+        M (int): number of microphones
+        d (float): distance between microphones (m)
+        v (float): speed of sound (m/s)
+        f0 (float): frequency of wave (Hz)
+
+    Returns:
+        np.ndarray: The array response
+    """  
+    """okay"""
+    result = np.array([ np.exp((0 -1*mic*(d/v)*np.sin(theta*np.pi/180)*2*np.pi*f0 *1j)) for mic in range (M)])
+
+    return result
+
+def generate_source(N):
+    
+    
+    s_real = np.random.randn(N)
+    s_imag = np.random.randn(N)
+    
+    
+    s = s_real + 1j * s_imag
+    
+    
+    # Power is defined as E[|s|^2], estimated by mean(|s|^2)
+    current_power = np.mean(np.abs(s)**2)
+    s_normalized = s / np.sqrt(current_power)
+    
+    return s_normalized
+
+def datamodel (M, N, theta0):
+    
+    theta = theta0
+    d = 0.1
+    v = 343
+    f0 = 250
+
+    A = np.array([a_lin(theta0[i], M, d, v, f0) for i in range (len(theta0)) ] )
+    A = np.transpose(A)
+    S = np.array([generate_source(N) for i in range (len(theta0))])
+    print(A.shape)
+    print(S.shape)
+    X = np.matmul(A,S)
+    print (np.linalg.matrix_rank(X))
+    return X
+
+if __name__ == "__main__":
+    M = 7
+    N = 6
+    theta0 = [10,22,11,40,-10,56]
+    X = datamodel(M, N, theta0)
+    rankx = np.linalg.matrix_rank(X)
+    print(rankx)
+    print (X.shape)
+    U, S, Vh = np.linalg.svd(X)
+    V = Vh.conj().T
+    print (S.shape)   
