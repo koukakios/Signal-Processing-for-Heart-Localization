68dd1342d5d71b7c3618836a2f0a2d30cad5cd8f
Gerrald <gerrald@weelden.net>
2025-12-10 16:00:46 +0100

    Cleaned up assignment 5.3.1

--- src/module_2/assignment531.py
+++ src/module_2/assignment531.py
@@ -1,95 +1,10 @@
 import numpy as np
 from lib.config.ConfigParser import ConfigParser
 from copy import deepcopy
-from lib.model.Model import Model
 import matplotlib.pyplot as plt
 from lib.model.generate import *
+from lib.model.Model_3D import Model_3D
 
-valve_locs = np.array([(6.37,10.65,6.00), # M
-              (0.94,9.57,5.5), # T
-              (5.5,11.00,3.6), # A
-              (3.9,11.5,4.5)]) # P
-
-
-mic_locs = np.array([(2.5, 5, 0), # 0
-            (2.5, 10, 0), # 1
-            (2.5, 15, 0), # 2
-            (7.5, 5, 0), # 3
-            (7.5, 10, 0), # 4
-            (7.5, 15, 0)]) # 5
-
-def log(msg):
-    """
-    @author: Gerrald
-    @date: 10-12-2025
-    """
-    if False:
-        print(msg)
-
-def threeD_model():
-    """
-    @author: Gerrald
-    @date: 10-12-2025
-    """
-    config = ConfigParser()
-    
-    model = Model(config)
-    model.import_csv(".\\src\\module_2\\model_params.csv")
-    # model.set_n(config.HeartSoundModel.NBeats)
-    model.set_n(10)
-    
-    valves = model.valves
-    
-    valves_init = np.array([valve.num_values() for valve in valves])
-    
-    signals = []
-    Fs = config.HeartSoundModel.Fs
-
-    
-    for mic_loc in mic_locs:
-        valves = deepcopy(valves_init)
-        log(valves)
-        new_mic_loc = np.tile(mic_loc,(4,1)) # repeat the mic loc, to comply with the dimensions of np.linalg.norm
-        
-        dists_to_valves = np.linalg.norm(valve_locs-new_mic_loc, axis=1)
-        
-        # calc delays and gains using distances to the valves
-        delays = dists_to_valves/config.Multichannel.V_body
-        gains = 1/dists_to_valves
-        # normalized the gains
-        gains_normalized = gains/max(gains)
-    
-        log(f"{delays},{gains_normalized}")
-        # Add delays
-        valves[:,0] += delays
-        # Adjust for gains
-        valves[:,3] *= gains_normalized
-        valves[:,4] *= gains_normalized
-        
-        log(valves)
-        
-        valvesParams = [
-            ValveParams(
-                name="",
-                delay_ms=float(valve[0])*1000,
-                duration_total_ms=float(valve[1])*1000,
-                duration_onset_ms=float(valve[2])*1000,
-                a_onset=float(valve[3]),
-                a_main=float(valve[4]),
-                ampl_onset=float(valve[5]),
-                ampl_main=float(valve[6]),
-                freq_onset=float(valve[7]),
-                freq_main=float(valve[8])
-            ) for valve in valves
-        ]
-        
-        model.valves = valvesParams
-        
-        _, signal = model.generate_model()
-        
-        signals.append(signal)
-        
-    return signals, Fs
 
 def plot(signals, Fs):
     """
@@ -105,5 +20,7 @@ def plot(signals, Fs):
 
 
 if __name__ == "__main__":
-    signals, Fs = threeD_model()
+    config = ConfigParser()
+    model = Model_3D(config, reduce_n=True)
+    signals, Fs = model.generate()
     plot(signals, Fs)
