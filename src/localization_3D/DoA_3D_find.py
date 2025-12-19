52b00c77f9c9715d1bdfaebd97c2ee03d0cdccbf
Kostas Koukakis <kkoukakis@tudelft.nl>
2025-12-19 09:36:19 +0100

    Also works for 2 sources

--- src/localization_3D/DoA_3D_find.py
+++ src/localization_3D/DoA_3D_find.py
@@ -18,31 +18,31 @@ if __name__ == "__main__":
     fs = 48000
     win = ('gaussian', 1e-2 * fs)
     SFT = ShortTimeFFT.from_window(win, fs, nperseg = 256 ,noverlap=0, scale_to='magnitude', phase_shift=None)
-    #path2source = Path(r"C:\Users\kkouk\IP3\2 source, distance 7 meter, microphone stand at 0 degrees, speaker at 7 degrees left and right.wav")
-    filepath1 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-60-degrees\recording_2024-09-30_12-58-35_channel_1.wav")
-    filepath2 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-60-degrees\recording_2024-09-30_12-58-35_channel_2.wav")
-    filepath3 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-60-degrees\recording_2024-09-30_12-58-35_channel_3.wav")
-    filepath4 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-60-degrees\recording_2024-09-30_12-58-35_channel_4.wav")
-    filepath5 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-60-degrees\recording_2024-09-30_12-58-35_channel_5.wav")
-    filepath6 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-60-degrees\recording_2024-09-30_12-58-35_channel_6.wav")
+    path2source = Path(r"C:\Users\kkouk\IP3\2 source, distance 7 meter, microphone stand at 0 degrees, speaker at 7 degrees left and right.wav")
+    #filepath1 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-60-degrees\recording_2024-09-30_12-58-35_channel_1.wav")
+    #filepath2 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-60-degrees\recording_2024-09-30_12-58-35_channel_2.wav")
+    #filepath3 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-60-degrees\recording_2024-09-30_12-58-35_channel_3.wav")
+    #filepath4 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-60-degrees\recording_2024-09-30_12-58-35_channel_4.wav")
+    #filepath5 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-60-degrees\recording_2024-09-30_12-58-35_channel_5.wav")
+    #filepath6 = Path(r"C:\Users\kkouk\IP3\Project-Heart-EE2L1\samples\Linear array sample recordings\LinearArray-60-degrees\recording_2024-09-30_12-58-35_channel_6.wav")
     
-    #sources, rate = sf.read(path2source)
-    rate, signal1 = wavfile.read(filepath1)
-    rate, signal2 = wavfile.read(filepath2)
-    rate, signal3 = wavfile.read(filepath3)
-    rate, signal4 = wavfile.read(filepath4)
-    rate, signal5 = wavfile.read(filepath5)
-    rate, signal6 = wavfile.read(filepath6)
+    sources, rate = sf.read(path2source)
+    #rate, signal1 = wavfile.read(filepath1)
+    #rate, signal2 = wavfile.read(filepath2)
+    #rate, signal3 = wavfile.read(filepath3)
+    #rate, signal4 = wavfile.read(filepath4)
+    #rate, signal5 = wavfile.read(filepath5)
+    #rate, signal6 = wavfile.read(filepath6)
     print ("ayo")
-    #print (sources.shape)
-    print(signal1.shape)
+    print (sources.shape)
+    #print(signal1.shape)
 
-    #signal1 = sources[:,0]
-    #signal2 = sources[:,1]
-    #signal3 = sources[:,2]
-    #signal4 = sources[:,3]
-    #signal5 = sources[:,4]
-    #signal6 = sources[:,5]
+    signal1 = sources[:,0]
+    signal2 = sources[:,1]
+    signal3 = sources[:,2]
+    signal4 = sources[:,3]
+    signal5 = sources[:,4]
+    signal6 = sources[:,5]
     
 
     
@@ -62,7 +62,7 @@ if __name__ == "__main__":
 
     Delta_f = f_bins[1] - f_bins[0]
     print( Delta_f)
-    bin = 9
+    bin = 16
     central_freq = bin*Delta_f
     X = Sx_all[:,bin , :]
     print(X.shape)
